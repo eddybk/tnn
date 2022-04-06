eb344f32681b1b671e8ff18917b883c9f56ca1f8
benjamingertrude01 <benjamingertrude01@gmail.com>
2022-04-06 18:43:17 +0200

    Added Vector struct

---
+++ tnn/Vector.cs
@@ -0,0 +1,22 @@
+﻿namespace tnn
+{
+    struct Vector
+    {
+        private List<float> _Data;
+        public Vector(float[] data)
+        {
+            _Data = new List<float> { data[0] };
+        }
+        public Vector(List<float> data)
+        {
+            _Data = data;
+        }
+        public List<float> AsList() => _Data;
+        public Matrix ToRowMatrix() => new Matrix(_Data.Count, 1).Set(_Data.Select(x => new List<float>(new float[] { x })).ToList())!;
+        public Matrix ToColumnMatrix() => new Matrix(1, _Data.Count).Set(new float[][] { _Data.ToArray() })!;
+        public float? Dot(Vector other)
+        {
+            return null;
+        }
+    }
+}
