4febfcbd095531ce186c878d91704e46441825c5
benjamingertrude01 <benjamingertrude01@gmail.com>
2022-04-06 17:04:13 +0200

    added Matrix struct

---
+++ tnn/Matrix.cs
@@ -0,0 +1,14 @@
+﻿namespace tnn
+{
+    struct Matrix
+    {
+        public (int, int) Shape { get; }
+        private List<List<float>> _Data;
+        public Matrix(int rows, int cols)
+        {
+            Shape = (rows, cols);
+            _Data = Enumerable.Range(0, rows).Select(i => Enumerable.Range(0, cols).Select(j => 0.0f).ToList()).ToList();
+        }
+        public List<List<float>> ToList() => _Data;
+    }
+}
